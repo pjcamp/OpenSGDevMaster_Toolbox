3c17e8375d169ac2ef7071b86e17bc29ca80cb49
David Kabala <djkabala@gmail.com>
2010-08-29 23:40:34 -0500

    Fixed: VisitSubTree was not correctly taking into account the transform of the parent node to the SubTreeNode in the adjustVolume function.
    Changed: VisitSubTree now VisitSubTreeMask as the traversal mask,
    previously it was seting the traversal mask to the bitwise and of the
    current traversal mask and VisitSubTreeMask.

--- Source/System/NodeCores/Groups/Misc/OSGVisitSubTree.cpp
+++ Source/System/NodeCores/Groups/Misc/OSGVisitSubTree.cpp
@@ -142,6 +142,13 @@ void VisitSubTree::adjustVolume(Volume &volume)
     {
         getSubTreeRoot()->updateVolume();
 
+        //The transform of the parent of the subTreeRoot needs to be removed from the volume transform
+        if(getSubTreeRoot()->getParent() != NULL)
+        {
+            Matrixr InvParentMat(getSubTreeRoot()->getParent()->getToWorld());
+            InvParentMat.invert();
+            volume.transform(InvParentMat);
+        }
         volume.extendBy(getSubTreeRoot()->getVolume());
     }
 }
@@ -171,6 +178,7 @@ ActionBase::ResultE VisitSubTree::renderEnter(Action *action)
         default:
             break;
     }
+    a->setTravMask(_sfSubTreeTravMask.getValue());
 
     if(this->getSubTreeRoot() != NULL && a->isVisible(this->getSubTreeRoot()))
     {
